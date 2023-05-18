78cf18d6a3ceeab37a2e167f4442c5e692661f1c
G284112022 <228411@st.takushoku-u.ac.jp>
2023-05-18 16:49:07 +0900

    課題4−1

--- kazuate.js
+++ kazuate.js
@@ -4,36 +4,52 @@ console.log('答え（デバッグ用）: ' + kotae);
 
 // 入力回数（予想回数）
 let kaisu = 0;
-
+let k=document.querySelector('span#kaisu'); 
+let an=document.querySelector('span#answer'); 
+let re=document.querySelector('p#result'); 
 // 予想を4回実行する
 // 将来以下の hantei(); の4回の呼び出しを全て削除する
 // 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
 hantei();
+/*hantei();
 hantei();
 hantei();
-hantei();
+*/
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
+
   
 for(kaisu;kaisu<4||kotae===yoso;){
     kaisu++;
-    console.log(kaisu+"回目の予想:"+yoso);
-    if(kaisu>3){
+    k.textContent = kaisu; //回数の測定
+    //console.log(kaisu+"回目の予想:"+yoso);
+    an.textContent = yoso; 
+
+    if(kaisu<3){
         console.log("答えは"+kotae+"でした"+"すでにゲームは終わっています。");
-    }
-    else if(yoso>kotae){
-        console.log("間違い、答えはもっと小さいですよ");
-    }
-    else if(yoso<kotae){
-        console.log("間違い、答えはもっと大きいですよ");
+        re.textContent = '答えは'+kotae+'でした。　すでにゲームは終わっています。'; 
+
     }
     else if(kotae===yoso){
         console.log(kaisu+"回目の予想:"+yoso);
         console.log("答えは"+kotae+"でした"+"すでにゲームは終わっています。");
+       
+        re.textContent = '正解です。おめでとう！'; 
     }
+    else if(yoso>kotae){
+        console.log("間違い、答えはもっと小さいですよ");
+        re.textContent = '間違い、答えはもっと小さいですよ'; 
+
+    }
+    else if(yoso<kotae){
+        console.log("間違い、答えはもっと大きいですよ");
+        re.textContent = '間違い、答えはもっと大きいですよ'; 
+
+    }
+   
 
 }
    
