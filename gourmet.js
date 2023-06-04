2973a4b466729d81b45e99bceba343c0e221b371
G284112022 <228411@st.takushoku-u.ac.jp>
2023-06-05 02:10:04 +0900

    テーブル生成できるようになったまだ店の名前等の分類分けはできてない

--- gourmet.js
+++ gourmet.js
@@ -1,240 +1,60 @@
-let data = {
-  "results": {
-    "api_version": "1.26",
-    "results_available": 52,
-    "results_returned": "10",
-    "results_start": 1,
-    "shop": [
-      {
-        "access": "京王八王子駅を背にし右手に見えるローソンの隣のビル、ホテルザ・ビーの2階です。",
-        "address": "東京都八王子市明神町４-6-12　ホテル・ザ・ビー八王子（旧八王子プラザホテル）2F",
-        "band": "可",
-        "barrier_free": "なし",
-        "budget": {
-          "average": "ディナー3000円",
-          "code": "B003",
-          "name": "3001～4000円"
-        },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "【サプライズ演出有】 結婚パーティー受付中",
-        "charter": "貸切可 ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア！50型モニター・スポットライト等充実",
-        "child": "お子様連れOK",
-        "close": "不定休有り。店休時は店長携帯電話09035324825まで問い合わせください♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ000989843/scoupon/?vos=nhppalsa000016"
-        },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり",
-        "free_food": "なし",
-        "genre": {
-          "catch": "大人の社交場非日常を味わうダイニング",
-          "code": "G001",
-          "name": "居酒屋"
-        },
-        //別の店
-        "horigotatsu": "なし",
-        "id": "J000989843",
-        "karaoke": "あり",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
-        },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
-        },
-        "lat": 35.6585460152,
-        "lng": 139.34327231,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/21/04/P038512104/P038512104_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
-        },
-        "midnight": "営業していない",
-        "mobile_access": "京王八王子駅1分/JR八王子駅北口5分",
-        "name": "バグダッドカフェ Bagdadcafe/モータウン MOTOWN",//店の名前
-        "name_kana": "ばぐだっどかふぇもーたうんはちおうじ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～21:00 （料理L.O. 20:00 ドリンクL.O. 20:00）",
-        "other_memo": "ステージ・マイク・カラオケ等",
-        "parking": "なし",
-        "party_capacity": 250,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_100.jpg"
-          },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_58_s.jpg"
-          }
-        },
-        "private_room": "あり ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
-        },
-        "shop_detail_memo": "ご質問、ご要望はお気軽にお問い合わせください♪",
-        "show": "あり",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
-        },
-        "station_name": "京王八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
-        },
-        "tatami": "なし",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/?vos=nhppalsa000016"
-        },
-        "wedding": "大歓迎！ステージ・マイク・音響・映像等、設備充実！！最大200名様まで是非どうぞ★",
-        "wifi": "あり"
-      },
-      //別の店
-      {
-        "access": "ＪＲ八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "address": "東京都八王子市東町12-14",
-        "band": "不可",
-        "barrier_free": "あり ：スタッフがお手伝いさせて頂きます。ご不明な点等お気軽にお声掛け下さい。事前のお問い合わせも◎",
-        "budget": {
-          "average": "2,000円(通常平均) 3,000円(宴会平均) ",
-          "code": "B002",
-          "name": "2001～3000円"
-        },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "2.5時間飲み放題付2000円~ 【★少人数様ソファー★】",
-        "charter": "貸切可 ：八王子で人気の個室バル最大50名様まで対応できます。20名様～フロア貸切応相談　最大100名様までOK",
-        "child": "お子様連れ歓迎 ：チャイルドシートもご用意しています。お子様連れのママ会・ご家族でのご利用にもお気軽にどうぞ♪",
-        "close": "【年中無休】貸切宴会のご予約も絶賛受付中！最大100名様までご利用可能ですので、会社宴会 同窓会 宴会にオススメ♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ001275177/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ001275177/scoupon/?vos=nhppalsa000016"
-        },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "free_food": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "genre": {
-          "catch": "八王子 個室居酒屋 飲み放題 肉バル 女子会",
-          "code": "G001",//"genre"code
-          "name": "居酒屋"
-        },
-        "horigotatsu": "なし ：温かな照明と開放的な店内で楽しくご宴会♪八王子での宴会 飲み会 女子会 二次会 合コンに◎",
-        "id": "J001275177",
-        "karaoke": "なし",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
-        },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
-        },
-        "lat": 35.6582259169,
-        "lng": 139.3381858122,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/61/98/P038366198/P038366198_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
-        },
-        "midnight": "営業している",
-        "mobile_access": "JR八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "name": "隠れ家バル Funny&Bouquet",//店の名前
-        "name_kana": "かくれやばる　ふぁにーあんどぶーけ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～翌5:00 （料理L.O. 翌3:00 ドリンクL.O. 翌4:00）",
-        "other_memo": "個室や貸切の詳細等お気軽にお電話にてお問い合わせください。八王子での飲み会に◎",
-        "parking": "なし ：お近くのコインパーキングをご利用下さい。",
-        "party_capacity": 50,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_100.jpg"
-          },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_58_s.jpg"
-          }
-        },
-        "private_room": "なし ：個室　2名×4室　4名×8室　8名～10名×4室　20名～40名×2室",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
-        },
-        "shop_detail_memo": "★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "show": "なし",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
-        },
-        "station_name": "八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
-        },
-        "tatami": "なし ：八王子で人気のお座敷個室！最大50名様まで対応できます。",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ001275177/?vos=nhppalsa000016"
-        },
-        "wedding": "八王子での結婚式の二次会や同窓会、打ち上げパーティー等多様なシーンにご利用下さい",
-        "wifi": "あり"
-      }
-    ]
-  }
-};
+let data ;
+let genre;
+let omise = [];
+let ob=0;
+let obleng=0;
 
-/////////// 課題3-2 ここからプログラムを書こう
-for(let n of data.results.shop){
-  console.log(n.name);
-}//コンソール部分
+
+//コンソール部分
 let b=document.querySelector('button#btn'); //ボタンを押した？
 b.addEventListener('click', showSelectResult); //ボタンを押したら起こすこと
 //指定したジャンルを調べる
 function showSelectResult() {
   let s = document.querySelector('select#janru');
   let idx = s.selectedIndex;  // idx 番目の option が選択された
+ 
+console.log(idx);
 
-  let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
-  let o = os.item(idx);       // os の idx 番目の要素
+  //let os = s.querySelectorAll('option');  // s の子要素 option をすべて検索
+  if(idx<10){
+    genre = 'G00'+idx; 
+  }
+  else{
+    genre = 'G0'+idx;
+  }
+  sendRequest();
 
+
+  
+
+  
+  //オブジェクトを配列omiseに入れる。
+  
+  
+
+       // os の idx 番目の要素
+  /*
   console.log(data.results.shop[0].name);//０個目のshopのnameを出力している。
   console.log('選択された ' + idx + ' 番目の option の情報:');
   console.log('  value=' + o.getAttribute('value'));  // id 属性を表示
   console.log('  textContent='+o.textContent);
-  
+  */
   //何を指定したかを見てそれに応じた情報を出す。
+  //ボタンを押したら一回消す。-
   
-    console.log('  textContent='+o.textContent);
 
-    let sho = document.querySelector('ul#shoping'); 
-    let x = idx-1;
-   // for(let v of data.results.shop){
-      let li = document.createElement('li');
-      let li2 = document.createElement('li');
-        li.textContent = data.results.shop[x].name;
+//     let sho = document.querySelector('ul#shoping'); 
+//     let x = idx-1;
+//    // for(let v of data.results.shop){
+//       let li = document.createElement('li');
+//       let li2 = document.createElement('li');
+//         li.textContent = data.results.shop[x].name;
         
-        sho.insertAdjacentElement('beforeend',li);
-        li2.textContent = data.results.shop[x].genre.catch;
-        sho.insertAdjacentElement('beforeend',li2);
- // } 
+//         sho.insertAdjacentElement('beforeend',li);
+//         li2.textContent = data.results.shop[x].genre.catch;
+//         sho.insertAdjacentElement('beforeend',li2);
+//   } 
+}
   
 
 
@@ -243,5 +63,125 @@ function showSelectResult() {
 //入力ちゅう//"genre"code=ジャンルコード
 
 
+
+
+
+
+// 通信を開始する処理
+function sendRequest() {
+    // URL を設定
     
+    let strat = 'https://www.nishita-lab.org/web-contents/jsons/hotpepper/';
+    let end = '.json';
+    let url = strat+genre+end;
+    console.log(url);
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
 }
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+     data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+
+    // data をコンソールに出力
+    console.log(data);
+
+    // data.x を出力
+    console.log(data.results.shop.genre.catch);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+
+  //アジャックスの中でないとエラーを吐く
+    ob=Object.keys(data.results.shop); 
+    obleng=ob.length;
+   for(let j=0; j<obleng;j++){
+
+    omise[j]=data.results.shop[j];//omise関数の中にdataのshopがある
+    console.log(omise[j]);
+   } 
+   
+   for(let All=0;All<obleng;All++){
+    console.log(All);
+
+    let div = document.querySelector('div#All');
+    let oj;
+    console.log(oj);
+     oj = document.createElement('table');
+     let tableid='oj'+All;//ここでidの数値を決めている。
+     oj.id=tableid;//ここでidを指定している
+
+     var tbtr = document.createElement("tr");
+     oj.insertAdjacentElement('beforeend',tbtr);
+
+     var name = document.createElement("td");
+     name.id='name';
+     tbtr.insertAdjacentElement('beforeend',name);
+    
+     let tr = document.querySelector('tr#name');//trはテーブルの内容
+     name.textContent = data.results.shop[All].name//nameをid(name)のtrに書き込んでる
+    
+
+    // on.insertAdjacentElement('beforeend',)
+     
+//thも入れていくようにする。あとtrにもidをつけて何を入れるかの指標にすること。id指定で入れるものを決められる。
+
+     var budget = document.createElement("td");
+     budget.id='budget';
+     tbtr.insertAdjacentElement('beforeend',budget);
+      tr = document.querySelector('tr#budget');
+     budget.textContent = data.results.shop[All].budget.name;
+
+     var access = document.createElement("td");
+     access.id = 'access';
+     tbtr.insertAdjacentElement('beforeend',access);
+     div.insertAdjacentElement('beforeend',oj);
+      tr = document.querySelector('tr#access');
+     access.textContent = data.results.shop[All].access;
+  }
+   
+  /* for(let tablenunber=1;tablenunber<obleng;tablenunber++){
+
+    var table = document.createElement("table");
+    table.id="oj";//ここを変数にできればいくつも作れる？
+
+    var headers= ["name","access","budget.name"];
+    for(var i = 0; i<headers.length;i++){
+      var th = document.createElement("th");
+      th.id = headers[i];
+      th.textContent = "--";
+
+      var tr = document.createElement("tr");
+      table.appendChild(tr);
+    }
+    var container = document.getElementById("tableContainer");
+    container.appendChild(table);
+    //let tableId = "table#oj"+tablenunber;
+    //let sho = document.querySelectorALL(); ここでテーブルの指定をしていた（多分）
+    var name=document.getElementById("name");
+   name.innerText = omise[tablenunber].name;
+
+   var name=document.getElementById("access");
+   name.innerText = omise[tablenunber].access;
+   var name=document.getElementById("budget.name");
+   name.innerText = omise[tablenunber].budget.name;
+   }*/
+   
+}
+//ここまでアジャックス。
